6a30a8a04dba4ee688758ea82378d566e5ed80b6
feasthotpot <feast.hotpot@gmail.com>
2026-06-11 03:39:05 +0800

    Update clock.js

--- api/clock.js
+++ api/clock.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
   res.setHeader('Pragma', 'no-cache');
   
